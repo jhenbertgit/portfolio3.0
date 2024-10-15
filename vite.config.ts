b8a8d99cbaaa978128f7f2c62658561c1a73459b
Jhenbert <jvv202726@gmail.com>
2024-10-15 20:46:47 +0800

    fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { config } from 'dotenv';
+
+config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
@@ -7,5 +10,8 @@ export default defineConfig({
 		fs: {
 			allow: ['.data']
 		}
+	},
+	define: {
+		'process.env.GOOGLE_APPSCRIPT_URL': JSON.stringify(process.env.GOOGLE_APPSCRIPT_URL)
 	}
 });
